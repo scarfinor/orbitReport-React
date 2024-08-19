67b5532cc3a07b32034a53d4fba361bdc191a0fa
scarfinor <scarfinor@gmail.com>
2024-08-18 22:59:54 -0500

    git commit updated Table.jsx

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,17 +1,25 @@
 const Table = ({ sat }) => {
   return (
     <table>
-    <thead>
-     <tr>
-       <th>Header TBD</th>
-     </tr>
-     </thead>
-     <tbody>
-     <tr>
-       <td>Row Data TBD</td>
-     </tr>
-     </tbody>
-   </table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Type of Satellite</th>
+          <th>Launch Date</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        {sat.map(({ id, name, type, launchDate, operational }) => (
+          <tr key={id}>
+            <td>{name}</td>
+            <td>{type}</td>
+            <td>{launchDate}</td>
+            <td>{operational ? "Active" : "Inactive"}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 };
 
